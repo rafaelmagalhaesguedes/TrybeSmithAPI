a086a6d550bff0c6565cfe1478813a8fa183a04b
rafael <rafaelmagalhaes1@live.com>
2024-02-19 19:01:11 -0300

    feat: function getAllProducts created on services products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -4,8 +4,13 @@ import { Product } from '../types/Product';
 // add new product
 export const addProduct = async (product: Product): Promise<Product> => {
   const newProduct = await ProductModel.create(product);
-  
   return newProduct.get({ plain: true }) as Product;
 };
 
-export default { addProduct };
+// get all products
+export const getAllProducts = async (): Promise<Product[]> => {
+  const products = await ProductModel.findAll();
+  return products.map((product) => product.get({ plain: true })) as Product[];
+};
+
+export default { addProduct, getAllProducts };
