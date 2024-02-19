89654655a98fe3c456f4f6e7a9f87c0b318addd4
rafael <rafaelmagalhaes1@live.com>
2024-02-19 15:27:29 -0300

    test: unit tests for creating products on controller layer

--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,10 +2,11 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import { createProduct } from '../../../src/controllers/products.controller';
 
 chai.use(sinonChai);
 
-describe('ProductsController', function () {
+describe('Products Controller', function () {
   const req = {} as Request;
   const res = {} as Response;
 
@@ -13,6 +14,26 @@ describe('ProductsController', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     sinon.restore();
+  });
+
+  it('1. Should create a product successfully', async () => {
+    req.body = { name: 'Martelo do Chapolin Colorado', price: '1.000.000.000 peças de ouro', userId: 1 };
+    await createProduct(req, res);
+    
+    expect(res.json).to.have.been.calledWith(
+      sinon.match.has('id', sinon.match.number)
+        .and(sinon.match.has('name', 'Martelo do Chapolin Colorado'))
+        .and(sinon.match.has('price', '1.000.000.000 peças de ouro'))
+        .and(sinon.match.has('userId', 1))
+    );
+  });
+
+  it('2. Should return an error when trying to create a product without sending the data', async () => {
+    req.body = {};
+    await createProduct(req, res);
+    
+    expect(res.status).to.have.been.calledWith(500);
+    expect(res.json).to.have.been.calledWith({ message: 'Informe os dados para criar o produto' });
   });
 
 });
