a626b693af1ac9f31042c125c197995e00fd599a
rafael <rafaelmagalhaes1@live.com>
2024-02-20 13:23:26 -0300

    fix: httpStatusMap

--- src/utils/httpStatusMap.ts
+++ src/utils/httpStatusMap.ts
@@ -1,13 +1,13 @@
 type StatusKey =
-'SUCCESSFUL' | 'CREATED' | 'NOT_FOUND' | 'CONFLICT' | 'INVALID_VALUE' | 'UNAUTHORIZED';
+'SUCCESSFUL' | 'CREATED' | 'NOT_FOUND' | 'INVALID_VALUE' | 'UNAUTHORIZED' | 'INTERNAL_ERROR';
 
 const httpErrorMap: Record<StatusKey, number> = {
   SUCCESSFUL: 200,
   CREATED: 201,
   NOT_FOUND: 404,
-  CONFLICT: 409,
   INVALID_VALUE: 422,
   UNAUTHORIZED: 401,
+  INTERNAL_ERROR: 500,
 };
 
 const statusHTTP = (status: StatusKey): number => httpErrorMap[status] || 500;
