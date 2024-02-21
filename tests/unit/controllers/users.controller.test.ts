8d8150be8b6cb49f7c4c92490dc1796c8823171f
rafael <rafaelmagalhaes1@live.com>
2024-02-21 16:16:40 -0300

    test: create test to users controller and services

--- tests/unit/controllers/users.controller.test.ts
+++ tests/unit/controllers/users.controller.test.ts
@@ -2,6 +2,8 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import UserModel from '../../../src/database/models/user.model';
+import UsersController from '../../../src/controllers/users.controller';
 
 chai.use(sinonChai);
 
@@ -15,4 +17,55 @@ describe('UsersController', function () {
     sinon.restore();
   });
 
+  describe('getAllUsers', function () {
+    it('should return all users with their product IDs', async function () {
+      // Arrange
+      const mockUser: any = {
+        dataValues: {
+          id: 1,
+          username: "Rafael Guedes",
+          vocation: "Developer Fullstack",
+          level: 1,
+          password: "123456",
+          productIds: [
+            {
+              id: 1,
+              name: "Zoom",
+              price: "0",
+              userId: 1,
+            },
+            {
+              id: 2,
+              name: "Slack",
+              price: "0",
+              userId: 1,
+            }
+          ],
+        },
+      };
+      sinon.stub(UserModel, 'findAll').resolves([mockUser]);
+
+      // Act
+      await UsersController.getUsers(req, res);
+
+      // Assert
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith([{
+        username: 'Rafael Guedes',
+        productIds: [1, 2],
+      }]);
+    });
+
+    it('should return an error message when an error occurs', async function () {
+      // Arrange
+      sinon.stub(UserModel, 'findAll').rejects(new Error('Database error'));
+
+      // Act
+      await UsersController.getUsers(req, res);
+
+      // Assert
+      expect(res.status).to.have.been.calledWith(500);
+      expect(res.json).to.have.been.calledWith({ message: 'Database error' });
+    });
+  });
 });
--- tests/unit/services/users.service.test.ts
+++ tests/unit/services/users.service.test.ts
@@ -1,7 +1,67 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import UsersService from '../../../src/services/users.service';
+import UserModel from '../../../src/database/models/user.model';
 
 describe('UsersService', function () {
   beforeEach(function () { sinon.restore(); });
+
+  describe('getUsers', function () {
+    it('should return all users with their product IDs', async function () {   
+      // Arrange
+      const mockUser: any = {
+        dataValues: {
+          id: 1,
+          username: "Rafael Guedes",
+          vocation: "Developer Fullstack",
+          level: 1,
+          password: "123456",
+          productIds: [
+            {
+              id: 1,
+              name: "Zoom",
+              price: "0",
+              userId: 1,
+            },
+            {
+              id: 2,
+              name: "Slack",
+              price: "0",
+              userId: 1,
+            }
+          ],
+        },
+      };
+
+      sinon.stub(UserModel, 'findAll').resolves([mockUser]);
+      
+      // Act
+      const result = await UsersService.getAllUsers();
+
+      // Assert
+      expect(result.status).to.equal('SUCCESSFUL');
+      expect(result.type).to.equal('success');
+      expect(result.message).to.be.undefined;
+      expect(result.data).to.be.an('array');
+      expect(result.data).to.deep.equal([{
+          username: 'Rafael Guedes',
+          productIds: [1, 2],
+      }]);
+    });
+
+    it('should return an error message when an error occurs', async function () {
+      // Arrange
+      sinon.stub(UserModel, 'findAll').rejects(new Error('Database error'));
+      
+      // Act
+      const result = await UsersService.getAllUsers();
+
+      // Assert
+      expect(result.status).to.equal('NOT_FOUND');
+      expect(result.type).to.equal('error');
+      expect(result.data).to.be.undefined;
+      expect(result.message).to.equal('Database error');
+    });
+  });
 
 });
