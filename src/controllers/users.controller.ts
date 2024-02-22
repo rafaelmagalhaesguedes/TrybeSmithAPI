e7e777999061b644038e2e557b8b42418332b12c
rafael <rafaelmagalhaes1@live.com>
2024-02-22 13:09:19 -0300

    refactor: users.controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { getAllUsers } from '../services/users.service';
+import { UsersService } from '../services';
 import statusHTTP from '../utils/httpStatusMap';
 
-export const getUsers = async (req: Request, res: Response) => {
-  const { type, message, data } = await getAllUsers();
+const getUsers = async (req: Request, res: Response) => {
+  const { type, message, data } = await UsersService.getAllUsers();
 
   if (type === 'error') {
     return res.status(statusHTTP('INTERNAL_ERROR')).json({ message });
