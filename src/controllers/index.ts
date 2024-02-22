4a346f3fa31e127cade2ce16342581c072b61f46
rafael <rafaelmagalhaes1@live.com>
2024-02-22 13:08:48 -0300

    feat: create barrel to controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,9 @@
+import ProductsController from './products.controller';
+import UsersController from './users.controller';
+import LoginController from './login.controller';
+
+export {
+  ProductsController,
+  LoginController,
+  UsersController,
+};
