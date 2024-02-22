5eedaf6ae68aee7d79ec170034bcc052bff3e3ad
rafael <rafaelmagalhaes1@live.com>
2024-02-22 18:09:33 -0300

    fix: add user exist validate on addProduct function in product.service

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -2,16 +2,23 @@ import { Model } from 'sequelize';
 import ProductModel, { ProductInputtableTypes } from '../database/models/product.model';
 import { ServiceResponse } from '../types/ServiceResponse';
 import { Product } from '../types/Product';
+import UserModel from '../database/models/user.model';
 
 // add new product
 export const addProduct = async (product: Product):
 Promise<ServiceResponse<Model<Product, ProductInputtableTypes>>> => {
+  const userExists = await UserModel.findOne({ where: { id: product.userId } });
+
+  if (!userExists) {
+    return { status: 'INVALID_VALUE', message: '"userId" not found', type: 'error' };
+  }
+  
   try {
     const newProduct = await ProductModel.create(product);
     return { status: 'CREATED', data: newProduct, type: 'success' };
   } catch (error) {
     const newError = error as Error;
-    return { status: 'INTERNAL_ERROR', message: newError.message, type: 'error' };
+    return { status: 'INVALID_VALUE', message: newError.message, type: 'error' };
   }
 };
 
