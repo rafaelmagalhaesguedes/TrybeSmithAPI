fb36800828486a2a4c6a1a8b84d2f8181c692bb7
rafael <rafaelmagalhaes1@live.com>
2024-02-22 13:09:07 -0300

    refactor: products.controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
-import { addProduct, getAllProducts } from '../services/products.service';
+import { ProductsService } from '../services';
 import statusHTTP from '../utils/httpStatusMap';
 
 // add new product
-export const createProduct = async (req: Request, res: Response) => {
-  const { type, data, message } = await addProduct(req.body);
+const createProduct = async (req: Request, res: Response) => {
+  const { type, data, message } = await ProductsService.addProduct(req.body);
 
   if (type !== 'success') {
     return res.status(statusHTTP('UNAUTHORIZED')).json({ message });
@@ -14,8 +14,8 @@ export const createProduct = async (req: Request, res: Response) => {
 };
 
 // get all products
-export const getProducts = async (req: Request, res: Response) => {
-  const { type, data, message } = await getAllProducts();
+const getProducts = async (req: Request, res: Response) => {
+  const { type, data, message } = await ProductsService.getAllProducts();
 
   if (type !== 'success') {
     return res.status(statusHTTP('NOT_FOUND')).json({ message });
@@ -24,4 +24,4 @@ export const getProducts = async (req: Request, res: Response) => {
   return res.status(statusHTTP('SUCCESSFUL')).json(data);
 };
 
-export default { createProduct, getAllProducts };
+export default { createProduct, getProducts };
