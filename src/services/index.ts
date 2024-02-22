672b8d73d0e03c89fb1638c8f569cfecbe554569
rafael <rafaelmagalhaes1@live.com>
2024-02-22 13:18:01 -0300

    feat: create barrel to services

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import ProductsService from './products.service';
+import UsersService from './users.service';
+import LoginService from './login.service';
+
+export {
+  ProductsService,
+  LoginService,
+  UsersService,
+};
