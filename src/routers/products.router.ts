9b8c869f788689f32873178636d7b0877badf001
rafael <rafaelmagalhaes1@live.com>
2024-02-22 18:04:52 -0300

    feat: add middleware validates on create products router

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { ProductsController } from '../controllers';
+import { NameMiddleware, PriceMiddleware, UserIdMiddleware } from '../middlewares';
 
 const productRouter = Router();
 
-productRouter.post('/', ProductsController.createProduct);
+productRouter.post(
+  '/',
+  NameMiddleware.validateProductName,
+  UserIdMiddleware.validateProductUserId,
+  PriceMiddleware.validateProductPrice,
+  ProductsController.createProduct,
+);
+
 productRouter.get('/', ProductsController.getProducts);
 
 export default productRouter;
