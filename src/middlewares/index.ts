91db932fcf17731a64262d87acf5f1d7e122c958
rafael <rafaelmagalhaes1@live.com>
2024-02-22 18:03:45 -0300

    feat: create barrel on middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,11 @@
+import AuthMiddleware from './auth.middleware';
+import NameMiddleware from './name.middleware';
+import PriceMiddleware from './price.middleware';
+import UserIdMiddleware from './userId.middleware';
+
+export {
+  AuthMiddleware,
+  NameMiddleware,
+  PriceMiddleware,
+  UserIdMiddleware,
+};
