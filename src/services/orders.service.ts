148e9f0b1d9e20d3007393c8e2e4b8ccf1383b4c
rafael <rafaelmagalhaes1@live.com>
2024-02-22 22:56:02 -0300

    feat: validates UserId and ProductId before create new Order

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -34,12 +34,18 @@ Promise<ServiceResponse<Model<Orders, OrderInputtableTypes>[]>> => {
 
 const create = async (order: OrderInputtableTypes):
 Promise<ServiceResponse<Model<Orders, OrderInputtableTypes>>> => {
+  const user = await UserModel.findByPk(order.userId);
+  if (!user) return { status: 'NOT_FOUND', message: 'User not found', type: 'error' };
+  
+  const product = await ProductModel.findByPk(order.productId);
+  if (!product) return { status: 'NOT_FOUND', message: 'Product not found', type: 'error' };
+
   try {
     const newOrder = await OrderModel.create(order);
     return { status: 'CREATED', data: newOrder, type: 'success' };
   } catch (error) {
     const newError = error as Error;
-    return { status: 'CONFLICT', message: newError.message, type: 'error' };
+    return { status: 'INTERNAL_ERROR', message: newError.message, type: 'error' };
   }
 };
 
